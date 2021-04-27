925e66e7f6d62bceaa385d865489886483650c41
iFall-iFail <64719808+iFall-iFail@users.noreply.github.com>
2021-04-27 12:15:44 +0600

    Launched Base Version
    Added Base Files

---
+++ javascript.js
@@ -0,0 +1,25 @@
+
+function PlayKey(e)
+{
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+    if (!audio)
+    {
+        return;
+    }
+    
+    key.classList.add('playing');
+
+    audio.currentTime = 0;
+    audio.play();
+
+    setTimeout(function() {
+        key.classList.remove('playing');
+    }, 200);
+
+}
+
+
+
+window.addEventListener('keydown', PlayKey);
